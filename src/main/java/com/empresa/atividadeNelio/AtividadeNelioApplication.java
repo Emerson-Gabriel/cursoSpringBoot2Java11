f907d25f708ce9dc2c06d1c4ea46fcdf5b304554
Emerson Gabriel <emersong730@gmail.com>
2019-08-10 14:58:18 -0300

    Projeto criado

---
+++ src/main/java/com/empresa/atividadeNelio/AtividadeNelioApplication.java
@@ -0,0 +1,13 @@
+package com.empresa.atividadeNelio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadeNelioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadeNelioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/empresa/atividadeNelio/AtividadeNelioApplicationTests.java
@@ -0,0 +1,16 @@
+package com.empresa.atividadeNelio;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AtividadeNelioApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
