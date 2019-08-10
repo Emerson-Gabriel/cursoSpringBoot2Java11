4e2365bb6707950bdebd4c41a84b1d5ebdcf5415
Emerson Gabriel <emersong730@gmail.com>
2019-08-10 16:33:09 -0300

    Subindo aula 03-06

--- src/main/java/com/empresa/atividadeNelio/resources/UserResouce.java
+++ src/main/java/com/empresa/atividadeNelio/resources/UserResouce.java
@@ -1,19 +1,36 @@
 package com.empresa.atividadeNelio.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.empresa.atividadeNelio.entities.User;
+import com.empresa.atividadeNelio.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResouce {
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Emerson Gabriel", "emersong730@gmail.com", "545645646", "iftm");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
+		
+		return ResponseEntity.ok().body(list);
 	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	
 }
