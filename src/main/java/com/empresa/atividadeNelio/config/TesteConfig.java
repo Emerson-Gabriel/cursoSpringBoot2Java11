cfe7a4540b680fd85868bed3d43f3ae9f0a45c07
Emerson Gabriel <emersong730@gmail.com>
2019-08-24 10:43:36 -0300

    Aula Category

--- src/main/java/com/empresa/atividadeNelio/config/TesteConfig.java
+++ src/main/java/com/empresa/atividadeNelio/config/TesteConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.empresa.atividadeNelio.entities.Category;
 import com.empresa.atividadeNelio.entities.Order;
 import com.empresa.atividadeNelio.entities.User;
 import com.empresa.atividadeNelio.entities.enums.OrderStatus;
+import com.empresa.atividadeNelio.repositories.CategoryRepository;
 import com.empresa.atividadeNelio.repositories.OrderRepository;
 import com.empresa.atividadeNelio.repositories.UserRepository;
 
@@ -20,11 +22,16 @@ public class TesteConfig implements CommandLineRunner {
 	
 	@Autowired
 	private UserRepository userRepository;
-
+	@Autowired
 	private OrderRepository orderRepository;
-	
+	@Autowired
+	private CategoryRepository categoryRepository;
 	@Override
 	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
 		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
@@ -32,6 +39,8 @@ public class TesteConfig implements CommandLineRunner {
 		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1, OrderStatus.PAID);
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2, OrderStatus.WAITING_PAYMENT);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1, OrderStatus.WAITING_PAYMENT);
+		
+		categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3))  ;
 		
 		userRepository.saveAll(Arrays.asList(u1,u2));
 		
