27ce9e9e0d7af11371676265ebe32b422eace0b9
Emerson Gabriel <emersong730@gmail.com>
2019-09-14 09:28:08 -0300

    User update

--- src/main/java/com/empresa/atividadeNelio/services/UserService.java
+++ src/main/java/com/empresa/atividadeNelio/services/UserService.java
@@ -3,6 +3,7 @@ package com.empresa.atividadeNelio.services;
 import java.util.List;
 import java.util.Optional;
 
+import org.aspectj.apache.bcel.util.Repository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -30,6 +31,19 @@ public class UserService {
 	
 	public void delete(Long id) {
 		repository.deleteById(id);
+	}
+	
+	public User update(Long id, User obj) {
+		User entity = repository.getOne(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+	
+	public void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+		
 	}
 	
 }
